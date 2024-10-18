db7c3ac3f2186f173d012de85c3542a4b8ed9b3c
Lucas Sylvester <lucassylvester02@gmail.com>
2024-10-18 14:09:18 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,9 +9,9 @@ export default defineConfig({
     port: 8080,      // The port you want to use for your dev server
     strictPort: true, // Fail if port 8080 is not available
     hmr: {
-      protocol: 'wss', // Use 'wss' for secure connections if your site uses HTTPS
+      protocol: 'ws', // Use 'wss' for secure connections if your site uses HTTPS
       host: '192.168.1.6', // Your domain name or server's public IP
-      port: 443, // Typically 443 for HTTPS
+      port: 8080, // Typically 443 for HTTPS
     },
   },
 });
