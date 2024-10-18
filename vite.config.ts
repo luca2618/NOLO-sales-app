e5415f53b00310ed496ff1e4eee2f5bc0ef7c308
Lucas Sylvester <lucassylvester02@gmail.com>
2024-10-18 14:08:12 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     strictPort: true, // Fail if port 8080 is not available
     hmr: {
       protocol: 'wss', // Use 'wss' for secure connections if your site uses HTTPS
-      host: 'nolo.lucas-sylvester.dk', // Your domain name or server's public IP
+      host: '192.168.1.6', // Your domain name or server's public IP
       port: 443, // Typically 443 for HTTPS
     },
   },
