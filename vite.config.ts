322dc2ffb5aad8d4cf1d1db6128b7328c2ea4880
Lucas Sylvester <lucassylvester02@gmail.com>
2024-10-18 14:12:57 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,4 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0', // Allows external access to the development server
-    port: 8080,      // The port you want to use for your dev server
-    strictPort: true, // Fail if port 8080 is not available
-    hmr: {
-      protocol: 'ws', // Use 'wss' for secure connections if your site uses HTTPS
-      host: '192.168.1.6', // Your domain name or server's public IP
-      port: 8080, // Typically 443 for HTTPS
-    },
-  },
 });
