575ef6cd424eada7ded801d11a5c6dfb93780816
Lucas Sylvester <lucassylvester02@gmail.com>
2024-10-18 14:06:56 +0200

    connect

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Allows external access to the development server
+    port: 8080,      // The port you want to use for your dev server
+    strictPort: true, // Fail if port 8080 is not available
+    hmr: {
+      protocol: 'wss', // Use 'wss' for secure connections if your site uses HTTPS
+      host: 'nolo.lucas-sylvester.dk', // Your domain name or server's public IP
+      port: 443, // Typically 443 for HTTPS
+    },
+  },
 });
