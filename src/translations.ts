2a5bf4ef7d33c480390808047f5d2d11ace7c262
Lucas Sylvester <lucassylvester02@gmail.com>
2024-10-21 23:18:35 +0200

    Update translations.ts

--- src/translations.ts
+++ src/translations.ts
@@ -1,13 +1,13 @@
 export const translations = {
   en: {
-    title: 'Calculate NOLO potential here',
+    title: 'Calculate your NOLO potential',
     totalSales: 'Total sales of water and soft drinks (DKK)',
     avgPrice: 'Average price of water and soft drinks (DKK)',
     premiumPrice: 'Price of average premium non-alcoholic drink (DKK)',
     conversion: 'Sales conversion (%)',
     results: 'Results',
     estimatedSales: 'Estimated sales',
-    percentageIncrease: 'Percentage increase'
+    percentageIncrease: 'Percentage increase',
   },
   dk: {
     title: 'Beregn NOLO potentialet her',
@@ -17,6 +17,6 @@ export const translations = {
     conversion: 'Konversion af salg (%)',
     results: 'Resultater',
     estimatedSales: 'Estimeret salg',
-    percentageIncrease: 'Procentvis stigning'
-  }
+    percentageIncrease: 'Procentvis stigning',
+  },
 };
